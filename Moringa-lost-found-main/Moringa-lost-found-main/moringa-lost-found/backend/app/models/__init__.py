20f15a815307187e48961072951511b57aa5404f
ian441 <ian.ayiro@student.moringaschool.com>
2025-07-24 21:45:27 -0800

    Remove deleted backend and frontend files

--- Moringa-lost-found-main/Moringa-lost-found-main/moringa-lost-found/backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from app.extensions import db  # ✅ Use shared instance
-
-# Import all models so they are registered
-from .user import User
-from .item import Item
-from .report import Report, Claim, Comment, Reward
--- moringa-lost-found/backend/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
-
-# Import all models so they are registered when db.create_all() or migrations run
-from .user import User
-from .item import Item
-from .report import Report, Claim, Comment, Reward
-# Add others as needed
